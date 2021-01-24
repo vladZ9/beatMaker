d0f152ba8d42619b8a9d1acd6eb8da25e8b6aaa0
vladz96 <clauvlady@Gmail.com>
2021-01-24 01:36:34 +0000

    beatMaker

---
+++ app.js
@@ -0,0 +1,56 @@
+class drumKit {
+  constructor() {
+    this.playButton = document.querySelector(".play");
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 120;
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    //modulus operator , divides first nunmber by second and what is left is the value stored.
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
+
+    this.index++;
+    //how many times does that multiply into that number , when is finshed what is left is what we get
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+//this is gonna invoke the class above and generate all variables and methods inside it
+const drum = new drumKit();
+drum.pads.forEach((pad) => {
+  pad.addEventListener("click", drum.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+drum.playButton.addEventListener("click", () => {
+  drum.start();
+});
